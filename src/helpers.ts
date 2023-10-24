5cc16f952dc16fcc955d8f205d83955cb1bdf392
ing.dacastillop <ing.dacastillop@gmail.com>
2023-10-24 10:56:05 -0500

    :sparkles: Implement validator JSON

--- src/helpers.ts
+++ src/helpers.ts
@@ -28,3 +28,48 @@ export const evalFormStateValid = <T>(props: EvalProps<T>): EvalResult => {
     valid: errors.length === 0
   };
 };
+
+type ObjectJSON = Record<string, any>;
+
+type Validators<T extends ObjectJSON> = Partial<
+  Record<keyof T, ValidatorFn<T>[]>
+>;
+
+type ErrorsJSON = Record<string, ValidatorError[]>;
+
+interface ValidatorsResult {
+  errors: ErrorsJSON;
+  valid: boolean;
+}
+
+export const validateJSON = <T extends ObjectJSON>(
+  object: T,
+  validators: Validators<T>
+): ValidatorsResult => {
+  return Object.keys(validators).reduce(
+    (result, key) => {
+      const validatorsFn = validators[key];
+
+      const errorsObject =
+        validatorsFn?.reduce((errors, validator) => {
+          const error = validator(object[key]);
+
+          if (error) {
+            errors.push(error);
+          }
+
+          return errors;
+        }, [] as ValidatorError[]) || [];
+
+      const errors: ErrorsJSON = { ...result.errors };
+      errors[key] = errorsObject;
+
+      return {
+        ...result,
+        errors,
+        valid: result.valid && errorsObject.length === 0
+      };
+    },
+    { valid: true, errors: {} } as ValidatorsResult
+  );
+};
