5e9e212cac5ddd605416b06a373fa69fb3a6d628
ing.dacastillop <ing.dacastillop@gmail.com>
2026-03-17 09:53:00 -0500

    :memo: Enabled export type FormArray

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,9 @@ export {
 } from './form-array/form-array-control';
 export * from './form-array/form-array-group.type';
 export { FormArrayGroup, formArrayGroup } from './form-array/form-array-group';
-export { FormArrayList, formArrayList } from './form-array/form-array-list';
 export * from './form-array/form-array-list.type';
+export { FormArrayList, formArrayList } from './form-array/form-array-list';
+export * from './form-array/form-array.type';
 export { FormArray, formArray } from './form-array/form-array';
 export * from './form-group/form-group.type';
 export { FormControls, FormGroup, formGroup } from './form-group/form-group';
